5f71f21d31676185d0235252b4155f53bd02a2bb
Lukas Drahnik <ldrahnik@gmail.com>
2026-05-12 09:12:51 +0200

    Init utils (print i2c scope device packages associated to laptop model names)

---
+++ utils/print_i2c_scope_device_packages.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+shopt -s nullglob
+
+for file in ${1:-data/*_Touchpad.dsl}; do
+    awk -v file="$file" '
+    BEGIN {
+        RS=""
+
+        name = file
+
+        sub(/^.*\//, "", name)
+        sub(/\.dsl$/, "", name)
+    }
+
+    {
+        while (match($0, /Name[[:space:]]*\(([A-Za-z0-9_]+)[^,]*,[[:space:]]*Package[[:space:]]*\((0x[0-9A-Fa-f]+)\)[[:space:]]*\{([^}]*)\}/, m)) {
+
+            body = m[3]
+
+            gsub(/\n/, " ", body)
+            gsub(/"/, "", body)
+            gsub(/,/, " ", body)
+            gsub(/[[:space:]]+/, " ", body)
+
+            n = split(body, arr, " ")
+
+            delete seen
+            out = ""
+            sep = ""
+
+            for (i = 1; i <= n; i++) {
+                if (arr[i] == "") continue
+                if (!(arr[i] in seen)) {
+                    seen[arr[i]] = 1
+                    out = out sep arr[i]
+                    sep = ","
+                }
+            }
+
+            printf "%s.dsl\t%s\n", name, out
+
+            $0 = substr($0, RSTART + RLENGTH)
+        }
+    }
+    ' "$file"
+done
